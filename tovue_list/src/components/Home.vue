017b20d5c7d8b83a25c21e988fb69095b73a2da7
DMilushev <d2@auxcode.com>
2021-09-04 23:50:08 +0300

    Created a vue project Fixed Vetur bug thinking it's a Vue 2 project

---
+++ tovue_list/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="tasks-container">
+        <div class="task-col"></div>
+        <div class="task-col"></div>
+        <div class="task-col"></div>
+    </div>
+</template>
+<style scoped>
+.tasks-container {
+    display: inline-grid;
+}
+</style>
